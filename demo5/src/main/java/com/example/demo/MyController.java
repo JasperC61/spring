2f6b61685ee9a87733a2d43c24c09e4d257aeb8c
JasperC61 <75925282+JasperC61@users.noreply.github.com>
2023-04-09 12:20:51 +0800

    response body transfer to JSON  application
    response body transfer to JSON

---
+++ demo5/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/product")
+    public Store product(){
+
+    Store store=new Store();
+    List<String> list=new ArrayList<>();
+    list.add("蘋果");
+    list.add("芭樂");
+    store.setProductList(list);
+        return store;
+    }
+
+
+    @RequestMapping("/user")
+    public Student user() {
+        Student student=new Student();
+        student.setName("Mary");
+        return student;
+    }
+}
