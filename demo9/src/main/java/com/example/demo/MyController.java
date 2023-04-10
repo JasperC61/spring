9131375995c9a7eec05360da9e5be914594c0728
JasperC61 <75925282+JasperC61@users.noreply.github.com>
2023-04-10 09:01:15 +0800

    @PathVariable application
    @PathVariable application

---
+++ demo9/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/t1")
+    public String t1(@RequestParam Integer id,
+                     @RequestParam (defaultValue = "Tim",name="t1name")String name){
+        System.out.println("id:"+id);
+        System.out.println("name:"+name);
+        return "hi t1";
+    }
+
+    @RequestMapping("/t2")
+    public String t2(@RequestBody Student student ){
+        System.out.println("student id:"+student.getId());
+        System.out.println("name:"+student.getName());
+        return "hi t2";
+    }
+    @RequestMapping("/t3")
+    public String t3(@RequestHeader String info){
+        System.out.println("header info:"+info);
+        return "h1 t3";
+    }
+    @RequestMapping("/t4/{Id}/{name}")
+    public String t4(@PathVariable Integer Id,
+                    @PathVariable String name){
+        System.out.println("path id:"+Id);
+        System.out.println(("path name:"+name));
+        return "h1 t4";
+    }
+
+}
